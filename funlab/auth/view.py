a3c8c1f0e924e4691f6c7ccda98bfbdfc22408af
Sunny Lin <sunnylin@mirle.com.tw>
2024-11-05 10:57:09 +0800

    ret: separate menus setup as a function setup_menus, and plugin to follow

--- funlab/auth/view.py
+++ funlab/auth/view.py
@@ -35,26 +35,30 @@ class AuthView(SecurityPlugin):
             msg = f'{oauth_name} OAuth register fail, please check config:{oauth_cfg}'
             raise e from Exception(msg)
         self.oauth_name_inuse:str = None
-        self.app.append_usermenu([MenuItem(title='Settings',
-                            icon='<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-settings" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">\
-                                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>\
-                                    <path d="M10.325 4.317c.426 -1.756 2.924 -1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543 -.94 3.31 .826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756 .426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543 -.826 3.31 -2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756 -2.924 1.756 -3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543 .94 -3.31 -.826 -2.37 -2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756 -.426 -1.756 -2.924 0 -3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94 -1.543 .826 -3.31 2.37 -2.37c1 .608 2.296 .07 2.572 -1.065z"></path>\
-                                    <path d="M9 12a3 3 0 1 0 6 0a3 3 0 0 0 -6 0"></path>\
-                                    </svg>',
-                            href=f'/settings'),
-                            MenuDivider(),
-                            MenuItem(title='Logout',
-                                icon='<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-logout" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">\
-                                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>\
-                                        <path d="M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2"></path>\
-                                        <path d="M9 12h12l-3 -3"></path>\
-                                        <path d="M18 15l3 -3"></path>\
-                                        </svg>',
-                                href=f'/logout'),
-                            ])
-
         self.register_routes()
         self.register_login_handler()
+
+    def setup_menus(self):
+        super().setup_menus()
+        self.app.append_usermenu([MenuItem(title='Settings',
+                    icon='<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-settings" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">\
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>\
+                            <path d="M10.325 4.317c.426 -1.756 2.924 -1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543 -.94 3.31 .826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756 .426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543 -.826 3.31 -2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756 -2.924 1.756 -3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543 .94 -3.31 -.826 -2.37 -2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756 -.426 -1.756 -2.924 0 -3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94 -1.543 .826 -3.31 2.37 -2.37c1 .608 2.296 .07 2.572 -1.065z"></path>\
+                            <path d="M9 12a3 3 0 1 0 6 0a3 3 0 0 0 -6 0"></path>\
+                            </svg>',
+                    href=f'/settings'),
+                    MenuDivider(),
+                    MenuItem(title='Logout',
+                        icon='<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-logout" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">\
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>\
+                                <path d="M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2"></path>\
+                                <path d="M9 12h12l-3 -3"></path>\
+                                <path d="M18 15l3 -3"></path>\
+                                </svg>',
+                        href=f'/logout'),
+                    ])
+
+
 
     @property
     def entities_registry(self):
@@ -72,6 +76,8 @@ class AuthView(SecurityPlugin):
 
     def get_userinfo_field_value(self, userinfo, fieldname):
         return userinfo[self.oauths.get(self.oauth_name_inuse).get('userinfo_key_mapping')[fieldname]]
+
+
 
     def register_routes(self):
         @self.blueprint.route('/login', defaults={'style': None}, methods=['GET', 'POST'])
